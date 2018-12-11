a871da03e331e17d515f4381b4cb1f97b3bc40d9
Stelil <deman666xxx@mail.ru>
2018-12-11 16:47:13 +0300

    skatch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,43 @@ import org.json.JSONObject;
 
 public class Main {
 
-    public void main(String[] args) {
+    static Tank firstTank, secondTank;
 
-        Tank firstTank = new Tank(80,
+    public static void main(String[] args) {
+
+        firstTank = new Tank(80,
                 30,
-                String.valueOf(constructorStrategy(1,2,3,4)),
+                String.valueOf(constructorStrategy(1, 2, 3, 4)),
                 3,
                 3);
 
-        Tank secondTank = new Tank(90,
+        secondTank = new Tank(90,
                 25,
-                String.valueOf(constructorStrategy(1,1,3,1)),
+                String.valueOf(constructorStrategy(1, 1, 3, 1)),
                 7,
                 7);
 
-        for (int x = 0; 10 > x; x++) {
-            for (int y = 1; 10 >= y; y++) {
+        int k = 0;
+        while (k++ < 4) {
+            picture(k);
+        }
+
+    }
+
+    private static JSONObject constructorStrategy(int left_move, int right_move, int up_move, int down_move) {
+        JSONArray ar = new JSONArray();
+        JSONObject obj = new JSONObject();
+        obj.put("left_move", 2);
+        obj.put("right_move", 1);
+        obj.put("up_move", 1);
+        obj.put("down_move", 3);
+        ar.put(obj);
+        return obj;
+    }
+
+    private static void picture(int i) {
+        for (int x = 10; x-- > 0; ) {
+            for (int y = 10; y-- > 0;) {
                 if (firstTank.getX() == x && firstTank.getY() == y) {
                     System.out.print("|1");
                 } else if (x == 0) {
@@ -30,24 +51,39 @@ public class Main {
 
                 }
             }
+
             System.out.println();
+        }
 
+        switch (i) {
+            case 1:
+                int getX = firstTank.getX();
+                int getMove = firstTank.getMove("left_move");
+                getX -= getMove;
+                firstTank.setX(getX);
+                break;
+            case 2:
+                getX = firstTank.getX();
+                getMove = firstTank.getMove("right_move");
+                getX += getMove;
+                firstTank.setX(getX);
+                break;
+            case 3:
+                int getY = firstTank.getY();
+                getMove = firstTank.getMove("up_move");
+                getY += getMove;
+                firstTank.setY(getY);
+                break;
+            case 4:
+                getY = firstTank.getY();
+                getMove = firstTank.getMove("down_move");
+                getY -= getMove;
+                firstTank.setY(getY);
+                break;
 
-            secondTank.setStrategy();
-            break;
         }
     }
 
-    private JSONObject constructorStrategy(int left_move, int right_move, int up_move, int down_move) {
-        JSONArray ar = new JSONArray();
-        JSONObject obj = new JSONObject();
-        obj.put("left_move", 2);
-        obj.put("right_move", 1);
-        obj.put("up_move", 1);
-        obj.put("down_move", 3);
-        ar.put(obj);
-        return obj;
-    }
 }
 
 
