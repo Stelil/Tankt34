4c7cdc5c1d35677df3e2bf5265c17f113a9083dc
Stelil <deman666xxx@mail.ru>
2018-12-13 12:31:52 +0300

    shoot

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -7,7 +7,7 @@ public class Action {
     int lengthMapX = 10;
     int lengthMapY = 10;
 
-    public void action(Tank tank) {
+    public void action(Tank tank, Tank enemy) {
         Random random = new Random();
         int line = random.nextInt(tank.getLengthStrategy());
 
@@ -17,11 +17,8 @@ public class Action {
         JSONArray array = obj.getJSONArray("strategy");
         org.json.JSONObject getObj = array.getJSONObject(line);
         String str = getObj.toString();
-
         //поворот против часовой стрелки
         if (str.toLowerCase().contains("left_turn".toLowerCase())) {
-            System.out.println("left");
-            System.out.println(tank.getX() + " " + tank.getY() + " " + tank.getDeg());
             int turn = getObj.getInt("left_turn");
             int getDeg = tank.getDeg();
             if (getDeg - turn < 0) {
@@ -31,13 +28,9 @@ public class Action {
                 getDeg -= turn;
                 tank.setDeg(getDeg);
             }
-            System.out.println(tank.getX() + " " + tank.getY() + " " + tank.getDeg());
         }
-
         //поворот по часовой стрелки
         if (str.toLowerCase().contains("right_turn".toLowerCase())) {
-            System.out.println("right");
-            System.out.println(tank.getX() + " " + tank.getY() + " " + tank.getDeg());
             int turn = getObj.getInt("right_turn");
             int getDeg = tank.getDeg();
             if (getDeg + turn >= 360) {
@@ -47,48 +40,35 @@ public class Action {
                 getDeg += turn;
                 tank.setDeg(getDeg);
             }
-            System.out.println(tank.getX() + " " + tank.getY() + " " + tank.getDeg());
         }
-
         // движение вперед
         if (str.toLowerCase().contains("up_move".toLowerCase())) {
             int getMove = getObj.getInt("up_move");
             int getX = tank.getX();
             int getY = tank.getY();
 
-            System.out.println("up");
-            System.out.println(tank.getX() + " " + tank.getY() + " " + tank.getDeg());
             int[] res = moving(tank.getDeg(), getX, getY, getMove);
             getX = res[0];
             tank.setX(getX);
             getY = res[1];
             tank.setY(getY);
-            System.out.println(tank.getX() + " " + tank.getY() + " " + tank.getDeg());
-
         }
-
         //движение назад
         if (str.toLowerCase().contains("down_move".toLowerCase())) {
             int getMove = getObj.getInt("down_move");
             int getX = tank.getX();
             int getY = tank.getY();
 
-            System.out.println("down");
-            System.out.println(tank.getX() + " " + tank.getY() + " " + tank.getDeg());
             int[] res = moving(tank.getDeg(), getX, getY, -getMove);
             getX = res[0];
             tank.setX(getX);
             getY = res[1];
             tank.setY(getY);
-            System.out.println(tank.getX() + " " + tank.getY() + " " + tank.getDeg());
-
         }
-
 
     }
 
     private int[] moving(int deg, int x, int y, int move) {
-        System.out.println(deg + " " + x + " " + y + " " + move);
         int[] res = new int[2];
         res[0] = x;
         res[1] = y;
@@ -147,4 +127,62 @@ public class Action {
         return res;
     }
 
+    public int[][] shoot(Tank firstTank, Tank secondTank, int lengthX, int lengthY) {
+        int[][] see = new int[lengthX][lengthY];
+        for (int x = 1; x < lengthX; x++) {
+            for (int y = 1; y < lengthY; y++) {
+                see[x][y] = 0;
+            }
+        }
+
+        if (firstTank.getX() == secondTank.getX() || firstTank.getY() == secondTank.getY()) {
+            if (firstTank.getX() < secondTank.getX() && firstTank.getDeg() == 180) {
+                for (int i = firstTank.getX(); i <= secondTank.getX(); i++) {
+                    see[i][firstTank.getY()] = 1;
+                }
+                secondTank.setHealth(secondTank.getHealth() - firstTank.getDamage());
+            } else if (firstTank.getX() > secondTank.getX() && firstTank.getDeg() == 0) {
+                for (int i = firstTank.getX(); i >= secondTank.getX(); i--) {
+                    see[i][firstTank.getY()] = 1;
+                }
+                secondTank.setHealth(secondTank.getHealth() - firstTank.getDamage());
+            } else if (firstTank.getY() < secondTank.getY() && firstTank.getDeg() == 90) {
+                for (int i = firstTank.getY(); i <= secondTank.getY(); i++) {
+                    see[firstTank.getX()][i] = 1;
+                }
+                secondTank.setHealth(secondTank.getHealth() - firstTank.getDamage());
+            } else if (firstTank.getY() > secondTank.getY() && firstTank.getDeg() == 270) {
+                for (int i = firstTank.getY(); i >= secondTank.getY(); i--) {
+                    see[firstTank.getX()][i] = 1;
+                }
+                secondTank.setHealth(secondTank.getHealth() - firstTank.getDamage());
+            }
+        }
+
+        if (secondTank.getX() == firstTank.getX() || secondTank.getY() == firstTank.getY()) {
+            if (secondTank.getX() < firstTank.getX() && secondTank.getDeg() == 180) {
+                for (int i = secondTank.getX(); i <= firstTank.getX(); i++) {
+                    see[i][secondTank.getY()] = 2;
+                }
+                firstTank.setHealth(firstTank.getHealth() - secondTank.getDamage());
+            } else if (secondTank.getX() > firstTank.getX() && secondTank.getDeg() == 0) {
+                for (int i = secondTank.getX(); i >= firstTank.getX(); i--) {
+                    see[i][secondTank.getY()] = 2;
+                }
+                firstTank.setHealth(firstTank.getHealth() - secondTank.getDamage());
+            } else if (secondTank.getY() < firstTank.getY() && secondTank.getDeg() == 90) {
+                for (int i = secondTank.getY(); i <= firstTank.getY(); i++) {
+                    see[secondTank.getX()][i] = 2;
+                }
+                firstTank.setHealth(firstTank.getHealth() - secondTank.getDamage());
+            } else if (secondTank.getY() > firstTank.getY() && secondTank.getDeg() == 270) {
+                for (int i = secondTank.getY(); i >= firstTank.getY(); i--) {
+                    see[secondTank.getX()][i] = 2;
+                }
+                firstTank.setHealth(firstTank.getHealth() - secondTank.getDamage());
+            }
+        }
+
+        return see;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,15 +23,55 @@ public class Main {
                 80,
                 30,
                 String.valueOf(constructorStrategy(90, 90, 1, 1)),
-                8,
+                3,
                 8,
                 constructorStrategy(90, 90, 3, 4).length(),
                 270);
 
+        game();
+    }
+
+    private static JSONObject constructorStrategy(int left_turn, int right_turn, int up_move, int down_move) {
+        JSONArray ar = new JSONArray();
+        JSONObject obj = new JSONObject();
+        obj.put("left_turn", left_turn);
+        ar.put(obj);
+
+        obj = new JSONObject();
+        obj.put("up_move", up_move);
+        ar.put(obj);
+
+        obj = new JSONObject();
+        obj.put("down_move", down_move);
+        ar.put(obj);
+
+        obj = new JSONObject();
+        obj.put("right_turn", right_turn);
+        ar.put(obj);
+
+        obj = new JSONObject();
+        obj.put("strategy", ar);
+
+        return obj;
+    }
+
+    private static void game() {
         int k = 0;
-        while (k++ < 99) {
-            for (int x = 1; x < lengthMapX; x++) {
-                for (int y = 1; y < lengthMapY; y++) {
+        boolean game = true;
+        while (game) {
+            k++;
+            System.out.println("STEP " + k);
+            System.out.println("1 health " + firstTank.getHealth());
+            System.out.println("2 health " + secondTank.getHealth());
+
+            new Action().action(firstTank, secondTank);
+            new Action().action(secondTank, firstTank);
+
+            int[][] sh;
+            sh = new Action().shoot(firstTank, secondTank, lengthMapX + 1, lengthMapY + 1);
+
+            for (int x = 1; x < lengthMapX + 1; x++) {
+                for (int y = 1; y < lengthMapY + 1; y++) {
                     if (firstTank.getX() == x && firstTank.getY() == y) {
                         switch (firstTank.getDeg()) {
                             case 0:
@@ -62,6 +102,8 @@ public class Main {
                                 System.out.print("|◀");
                                 break;
                         }
+                    } else if (sh[x][y] != 0) {
+                        System.out.print("|" + sh[x][y]);
                     } else if (y < lengthMapX) {
                         System.out.print("|_");
                     } else {
@@ -73,34 +115,10 @@ public class Main {
             }
             System.out.println();
 
-            new Action().action(firstTank);
-            new Action().action(secondTank);
+            if (firstTank.getHealth() <= 0 || secondTank.getHealth() <= 0) {
+                game = false;
+            }
         }
     }
-
-    private static JSONObject constructorStrategy(int left_turn, int right_turn, int up_move, int down_move) {
-        JSONArray ar = new JSONArray();
-        JSONObject obj = new JSONObject();
-        obj.put("left_turn", left_turn);
-        ar.put(obj);
-
-        obj = new JSONObject();
-        obj.put("up_move", up_move);
-        ar.put(obj);
-
-        obj = new JSONObject();
-        obj.put("down_move", down_move);
-        ar.put(obj);
-
-        obj = new JSONObject();
-        obj.put("right_turn", right_turn);
-        ar.put(obj);
-
-        obj = new JSONObject();
-        obj.put("strategy", ar);
-
-        return obj;
-    }
-
 
 }
